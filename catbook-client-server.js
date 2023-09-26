9cb72a0b8f1e35a9d0b06fbdba9acb013b518271
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-09-26 17:54:12 -0500

    fix: Add req to callback function for app.get('/*'... on server to fix production  TypeError 'res.sendFile is not a function'

--- catbook-client-server.js
+++ catbook-client-server.js
@@ -6,7 +6,7 @@ const app = express();
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/*', (res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
