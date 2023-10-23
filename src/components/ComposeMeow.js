149c5b50b8cfbd866c03424405b5d996eda44a0a
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-10-22 20:55:45 -0500

    chore: Add title and scale on hover to icons, and scale on hover and styling to buttons, in ComposeMeow component

--- src/components/ComposeMeow.js
+++ src/components/ComposeMeow.js
@@ -35,6 +35,8 @@ const ComposeMeow = ({
   const [selectedGif, setSelectedGif] = useState(null);
   const [selectedGifUrl, setSelectedGifUrl] = useState(null); //new
 
+  // const [isSelectingGif, setIsSelectingGif] = useState(false);
+  
   const [selectedFile, setSelectedFile] = useState(null);
   const [meowText, setMeowText] = useState('');
   const [embeddedMeowData, setEmbeddedMeowData] = useState(null);
@@ -341,9 +343,10 @@ const ComposeMeow = ({
 
   <div>
 { 
-      isEditing || isSelectingGif ? ( 
+      !isEditing && isSelectingGif ? ( 
         
-          <button onClick={closeGifSelect}>
+          <button onClick={closeGifSelect}
+          className='bg-purple-400 text-white rounded-full px-4 py-2 hover:scale-110 transition-all ease-in-out duration-200'>
             Close GIF Select
             </button>
         ) : null
@@ -390,7 +393,7 @@ const ComposeMeow = ({
     className="cursor-pointer"
     // className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer"
   >
-    <img src={mediaIcon} alt="Add Media" className='w-5'/>
+    <img src={mediaIcon} alt="Add Media" title='Add Media' className='w-8 hover:scale-110 transition-all ease-in-out duration-200'/>
   </label>
 </div>
       )
@@ -419,7 +422,7 @@ const ComposeMeow = ({
      
       {!isEditing && !isSelectingGif ? ( 
           <button onClick={openGifSelect}>
-            <img src={gifIcon} alt="Add GIF" className='w-6'/>
+            <img src={gifIcon} alt="Add GIF" title='Add GIF' className='w-8 hover:scale-110 transition-all ease-in-out duration-200'/>
             </button>
         ) : null
     
@@ -450,7 +453,10 @@ const ComposeMeow = ({
 
 
       {isEditing ? (
-      <button onClick={() => onUpdateMeow()}> Post Changes </button>
+      <button onClick={() => onUpdateMeow()}
+      className='bg-purple-400 text-white rounded-full px-4 py-2 hover:scale-110 transition-all ease-in-out duration-200'> 
+      Post Changes 
+      </button>
     ) : (
       !isSelectingGif && (
         <button
@@ -458,6 +464,7 @@ const ComposeMeow = ({
             console.log('Button Clicked');
             onCreateMeow();
           }}
+          className='bg-purple-400 text-white rounded-full px-4 py-2 hover:scale-110 transition-all ease-in-out duration-200'
         >
           Post
         </button>
