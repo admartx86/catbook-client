0917da91b52f52b837075e154fc44f069e197048
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-11-05 01:21:07 -0500

    refactor: Move Close GIF button to Gif component

--- src/components/ComposeMeow.js
+++ src/components/ComposeMeow.js
@@ -38,6 +38,7 @@ const ComposeMeow = ({
 
   const [selectedGif, setSelectedGif] = useState(null);
   const [selectedGifUrl, setSelectedGifUrl] = useState(null);
+
   const [selectedFile, setSelectedFile] = useState(null);
   const [meowText, setMeowText] = useState('');
   const [embeddedMeowData, setEmbeddedMeowData] = useState(null);
@@ -198,9 +199,6 @@ const ComposeMeow = ({
     setIsSelectingGif(true);
   };
 
-  const closeGifSelect = () => {
-    setIsSelectingGif(false);
-  };
 
   return (
     <section
@@ -236,27 +234,14 @@ const ComposeMeow = ({
           <Meow meow={originalMeow} isEmbedded={true} />
         </div>
       )}
-      <div className="flex flex-col pl-12">
-        <div className="flex p-3">
-          {!isEditing && isSelectingGif ? (
-            <button
-              onClick={closeGifSelect}
-              className="bg-purple-400 text-white 
-                rounded-full px-4 py-2
-                sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl
-                hover:scale-110 transition-all ease-in-out duration-200"
-            >
-              Close GIF Select
-            </button>
-          ) : null}
-        </div>
-        <div>
-          {isSelectingGif ? (
-            <Gif setSelectedGifUrl={setSelectedGifUrl} setIsSelectingGif={setIsSelectingGif} />
-          ) : null}
-        </div>
-      </div>
 
+      <Gif
+        setSelectedGif={setSelectedGif}
+        setSelectedGifUrl={setSelectedGifUrl}
+        setIsSelectingGif={setIsSelectingGif}
+        isEditing={isEditing}
+        isSelectingGif={isSelectingGif}
+      />
       <div className="flex flex-col lg:flex-row p-2">
         {isEditing ? <p>{renderMedia(meowMedia)}</p> : null}
         {isEditing && originalMeow && embeddedMeowData ? (
--- src/components/Gif.js
+++ src/components/Gif.js
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { Grid } from '@giphy/react-components';
 import { GiphyFetch } from '@giphy/js-fetch-api';
 
-const Gif = ({ setSelectedGifUrl, setIsSelectingGif }) => {
+const Gif = ({
+  setSelectedGif,
+  setSelectedGifUrl,
+  setIsSelectingGif,
+  isEditing,
+  isSelectingGif
+}) => {
   const [localSelectedGif, setLocalSelectedGif] = useState(null);
 
   const gf = new GiphyFetch(process.env.REACT_GIPHY_API_KEY);
@@ -23,7 +29,39 @@ const Gif = ({ setSelectedGifUrl, setIsSelectingGif }) => {
     }
   }, [localSelectedGif]);
 
-  return <Grid width={800} columns={3} fetchGifs={fetchGifs} onGifClick={handleGifClick} />;
+  const closeGifSelect = () => {
+    setIsSelectingGif(false);
+  };
+
+  return (
+    <div className="flex flex-col pl-12">
+      <div className="flex p-3">
+        {!isEditing && isSelectingGif ? (
+          <button
+            onClick={closeGifSelect}
+            className="bg-purple-400 text-white 
+            rounded-full px-4 py-2
+            sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl
+            hover:scale-110 transition-all ease-in-out duration-200"
+          >
+            Close GIF Select
+          </button>
+        ) : null}
+      </div>
+      <div>
+        {isSelectingGif ? (
+          <Grid
+            width={800}
+            columns={3}
+            fetchGifs={fetchGifs}
+            onGifClick={handleGifClick}
+            setSelectedGifUrl={setSelectedGifUrl}
+            setIsSelectingGif={setIsSelectingGif}
+          />
+        ) : null}
+      </div>
+    </div>
+  );
 };
 
 export default Gif;
